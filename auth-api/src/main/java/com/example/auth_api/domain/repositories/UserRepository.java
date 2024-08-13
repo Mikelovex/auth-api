d2b75dde25faeabe466293ad953a640c3901cb00
mikelovex <maicon.rafael17@gmail.com>
2024-08-13 20:01:25 -0300

    feat: Add findByEmail method

--- auth-api/src/main/java/com/example/auth_api/domain/repositories/UserRepository.java
+++ auth-api/src/main/java/com/example/auth_api/domain/repositories/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.auth_api.domain.repositories;
 import com.example.auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
